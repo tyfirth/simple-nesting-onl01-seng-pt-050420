b90b5fc85a9a915f16a7e023a58cd4af736973e3
Tyler Firth <tyfirth@gmail.com>
2020-05-15 03:28:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash{:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}}
+programmer_hash{:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}
 	programmer_hash = 
  		{
         :grace_hopper => {
